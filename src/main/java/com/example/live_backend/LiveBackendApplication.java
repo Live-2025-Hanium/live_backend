764cb858e3d3e1217ddc26058ad14e19c3811193
최윤정 <155123302+yunjeooong@users.noreply.github.com>
2025-05-22 12:45:37 +0900

    feat(common):BaseEntity 구현

--- src/main/java/com/example/live_backend/LiveBackendApplication.java
+++ src/main/java/com/example/live_backend/LiveBackendApplication.java
@@ -5,9 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LiveBackendApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(LiveBackendApplication.class, args);
+		System.out.println("Live Backend is running!");
 	}
 
 }
---
+++ src/main/java/com/example/live_backend/common/BaseEntity.java
@@ -0,0 +1,24 @@
+package com.example.live_backend.common;
+import java.time.LocalDateTime;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import lombok.Getter;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseEntity {
+    // Entity가 생성되어 저장될 때 시간이 자동 저장됩니다.
+    @CreatedDate
+    private LocalDateTime createdDate;
+
+    // 조회한 Entity 값을 변경할 때 시간이 자동 저장됩니다.
+    @LastModifiedDate
+    private LocalDateTime modifiedDate;
+
+    // 삭제 시점
+    private boolean isDeleted = false;
+}
