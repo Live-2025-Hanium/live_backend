86eef167c299a0ac633c8a40ecb8c8965e2e6cc3
최윤정 <155123302+yunjeooong@users.noreply.github.com>
2025-05-22 19:37:13 +0900

    feat: 전역 예외 처리 및 공통 컴포넌트 구현 예외 처리 구조(CustomException, ErrorCode, GlobalExceptionHandler)와 표준 응답용 ResponseHandler 등을 추가

---
+++ src/main/java/com/example/live_backend/common/exception/GlobalExceptionHandler.java
@@ -0,0 +1,52 @@
+package com.example.live_backend.common.exception;
+
+import com.example.live_backend.common.response.Message;
+import com.example.live_backend.common.response.ResponseHandler;
+import java.util.List;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+@Slf4j
+public class GlobalExceptionHandler {
+    @ExceptionHandler(value = CustomException.class)
+    public ResponseEntity<?> handleCustomException(CustomException exception) {
+        log.warn("[handleCustomException] : {} \n message: {}", exception.getErrorCode(),
+                exception.getMessage());
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(ResponseHandler.errorResponse(exception.getMessage(), exception.getErrorCode().name()));
+    }
+
+    @ExceptionHandler(value = MethodArgumentNotValidException.class)
+    public ResponseEntity<?> handleMethodArgumentException(MethodArgumentNotValidException exception) {
+        List<FieldErrorResponse> errors = exception.getBindingResult().getFieldErrors()
+                .stream().map(FieldErrorResponse::of)
+                .toList();
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(ResponseHandler.errorResponse(errors, Message.BAD_REQUEST.getDescription()));
+    }
+
+    @ExceptionHandler(value = Exception.class)
+    public ResponseEntity<?> handleServerException(Exception exception) {
+        // TODO: ServerError log message
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(ResponseHandler.errorResponse(exception.getMessage(), Message.SYSTEM_ERROR.getDescription()));
+    }
+
+    private record FieldErrorResponse(String field, String message, Object rejectedValue) {
+        public static FieldErrorResponse of(final FieldError fieldError) {
+            return new FieldErrorResponse(
+                    fieldError.getField(),
+                    fieldError.getDefaultMessage(),
+                    fieldError.getRejectedValue()
+            );
+        }
+    }
+}
---
+++ src/main/java/com/example/live_backend/common/response/ResponseHandler.java
@@ -0,0 +1,33 @@
+package com.example.live_backend.common.response;
+
+import java.time.LocalDateTime;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Builder
+@Data
+public class ResponseHandler<T> {
+    private final LocalDateTime timestamp = LocalDateTime.now();
+    private String message;
+    private T data;
+
+    public ResponseHandler(final String message, T data) {
+        this.message = message;
+        this.data = data;
+    }
+
+    public static <T> ResponseHandler<T> response(T data) {
+        return ResponseHandler.<T>builder()
+                .message(Message.SUCCESS.getDescription())
+                .data(data)
+                .build();
+    }
+
+    public static <T> ResponseHandler<T> errorResponse(T data, String message) {
+        return ResponseHandler.<T>builder()
+                .message(message)
+                .data(data)
+                .build();
+    }
+}
