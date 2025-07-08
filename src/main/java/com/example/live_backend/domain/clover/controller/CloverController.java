1a5fb67e5bb224addd599f10ba3e28f070e442fe
kae-mi <rudahyang7815@gmail.com>
2025-07-08 15:35:16 +0900

    클로버 미션 상세 조회 Controller 코드 추가

--- src/main/java/com/example/live_backend/domain/clover/controller/CloverController.java
+++ src/main/java/com/example/live_backend/domain/clover/controller/CloverController.java
@@ -1,6 +1,7 @@
 package com.example.live_backend.domain.clover.controller;
 
 import com.example.live_backend.domain.clover.dto.CloverMissionListResponseDto;
+import com.example.live_backend.domain.clover.dto.CloverMissionResponseDto;
 import com.example.live_backend.domain.clover.service.MissionService;
 import com.example.live_backend.global.error.response.ResponseHandler;
 import io.swagger.v3.oas.annotations.Operation;
@@ -28,6 +29,18 @@ public class CloverController {
         CloverMissionListResponseDto response = missionService.getCloverMissionList();
 
         log.info("클로버 미션 리스트 조회 완료 - 총 {}개 미션", response.getMissions().size());
+
+        return ResponseHandler.response(response);
+    }
+
+    @GetMapping("/{missionId}")
+    @Operation(summary = "클로버 미션(1개) 상세 조회 ",
+            description = "클로버 미션 1개의 상세 정보를 조회합니다.")
+    public ResponseHandler<CloverMissionResponseDto> getCloverMission(@PathVariable Long missionId) {
+
+        log.info("클로버 미션 상세 조회 API 호출");
+
+        CloverMissionResponseDto response = missionService.getCloverMission(missionId);
 
         return ResponseHandler.response(response);
     }
