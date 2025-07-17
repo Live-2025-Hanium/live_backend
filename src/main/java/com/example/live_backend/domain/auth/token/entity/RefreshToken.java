9c74e6f81828a9ce2e63fbc19d530f3c88dafca3
yunjeooong <yunjeongssu@gmail.com>
2025-07-17 21:56:52 +0900

    feat: refresh token entity에  명시적 컬럼명  제약조건 추가

--- src/main/java/com/example/live_backend/domain/auth/token/entity/RefreshToken.java
+++ src/main/java/com/example/live_backend/domain/auth/token/entity/RefreshToken.java
@@ -1,18 +1,20 @@
 package com.example.live_backend.domain.auth.token.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
+@Table(name = "refresh_tokens")
 @Getter
 @NoArgsConstructor
 public class RefreshToken {
 
 	@Id
+	@Column(name = "user_id", nullable = false)
 	private Long userId;
 
+	@Column(name = "token", nullable = false, length = 500)
 	private String token;
 
 	public RefreshToken(Long userId, String token) {
