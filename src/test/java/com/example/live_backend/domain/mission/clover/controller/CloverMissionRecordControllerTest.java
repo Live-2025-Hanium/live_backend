c5d0e0f61d9f000155a29378ef8e666ca207a4ef
kae-mi <rudahyang7815@gmail.com>
2025-08-22 17:29:47 +0900

    test: 클로버 미션 기록 서비스  단위 테스트 추가

--- src/test/java/com/example/live_backend/domain/mission/clover/controller/CloverMissionRecordControllerTest.java
+++ src/test/java/com/example/live_backend/domain/mission/clover/controller/CloverMissionRecordControllerTest.java
@@ -15,7 +15,6 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.assertj.core.api.FactoryBasedNavigableListAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.*;
