bcea78b519b98171f4e90d60ac5ab46c09223a0b
yunjeooong <yunjeongssu@gmail.com>
2025-09-11 22:10:09 +0900

    feat: 카카오 Local API Feign Client 구현
    - KakaoLocalFeign 인터페이스 구현
    - KakaoFeignConfig 설정 클래스 추가
    - 카카오 API 응답 DTO 추가
    - Application에 FeignClients, Caching 활성화

---
+++ src/main/java/com/example/live_backend/domain/places/infra/feign/dto/KakaoCategoryResponse.java
@@ -0,0 +1,20 @@
+package com.example.live_backend.domain.places.infra.feign.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class KakaoCategoryResponse {
+
+    @JsonProperty("documents")
+    private List<KakaoKeywordResponse.KakaoPlace> documents;
+
+    @JsonProperty("meta")
+    private KakaoKeywordResponse.Meta meta;
+}
