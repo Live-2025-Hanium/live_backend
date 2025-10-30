4fcce39749f7db34b571fb953d9d43f46c43d064
kae-mi <rudahyang7815@gmail.com>
2025-10-30 19:46:49 +0900

    refactor:   미션 상태 변경 로직 임시 변경

--- src/main/java/com/example/live_backend/domain/mission/clover/entity/CloverMissionRecord.java
+++ src/main/java/com/example/live_backend/domain/mission/clover/entity/CloverMissionRecord.java
@@ -143,9 +143,9 @@ public class CloverMissionRecord {
     }
 
     public void startMission() {
-        if (this.cloverMissionStatus != CloverMissionStatus.ASSIGNED && this.cloverMissionStatus != CloverMissionStatus.PAUSED) {
+        /*if (this.cloverMissionStatus != CloverMissionStatus.ASSIGNED && this.cloverMissionStatus != CloverMissionStatus.PAUSED) {
             throw new CustomException(ErrorCode.INVALID_MISSION_STATUS);
-        }
+        }*/
         this.cloverMissionStatus = CloverMissionStatus.STARTED;
     }
 
