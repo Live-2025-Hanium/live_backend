5cae3307ad24a003515137924f9ff98c871c44c9
kae-mi <rudahyang7815@gmail.com>
2025-11-18 18:19:19 +0900

    chore: Mock 빈에 @Configuration 적용

--- src/test/java/com/example/live_backend/config/TestVectorStoreConfig.java
+++ src/test/java/com/example/live_backend/config/TestVectorStoreConfig.java
@@ -4,15 +4,15 @@ import org.springframework.ai.document.Document;
 import org.springframework.ai.vectorstore.SearchRequest;
 import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.ai.vectorstore.filter.Filter;
-import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
 
 import java.util.Collections;
 import java.util.List;
 
-@TestConfiguration
+@Configuration
 @Profile("test")
 public class TestVectorStoreConfig {
 
